85870f411c70e218ebcd3b3deb9617a64974be0e
annagurri <106530217+annagurri@users.noreply.github.com>
2022-06-13 13:29:27 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+game.splash("\"Benvinguts a l'espai", "\"Apreta A per començar i B per disparar\"")
+effects.starField.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . . . . . c c c a a a . . . . . 
+    . . . . 8 8 3 3 3 1 a a . . . . 
+    . . 8 f f f c c a a f f a a . . 
+    . 8 8 8 8 a a 3 3 3 3 1 3 a a . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
+    `, SpriteKind.Player)
+mySprite.setPosition(73, 105)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(5)
